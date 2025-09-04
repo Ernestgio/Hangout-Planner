9571f28a3ee8a9e376ef651a6818db7c287f9ef9
Ernestgio <giovann.ernest@gmail.com>
2025-09-04 22:27:43 +0700

    echo setup

---
+++ cmd/Hangout/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func init() {
+	if os.Getenv("ENV") != "PROD" {
+		if err := godotenv.Load(); err != nil {
+			log.Printf("Warning: .env file not loaded: %v", err)
+		}
+	}
+}
+
+func main() {
+	server := echo.New()
+
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "9000"
+	}
+	server.Logger.Fatal(server.Start(":" + port))
+}
