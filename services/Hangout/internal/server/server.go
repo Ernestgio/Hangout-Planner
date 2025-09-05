bf01fa106f528c3e5c5f9b016c404381141aaba5
Ernestgio <giovann.ernest@gmail.com>
2025-09-05 16:13:33 +0700

    refactor: separate main and run, added graceful shutdown, added .vscode to gitignore debugger

--- services/Hangout/internal/server/server.go
+++ services/Hangout/internal/server/server.go
@@ -3,11 +3,12 @@ package server
 import (
 	"github.com/Ernestgio/Hangout-Planner/services/Hangout/internal/config"
 	"github.com/Ernestgio/Hangout-Planner/services/Hangout/logging"
+	"gorm.io/gorm"
 
 	"github.com/labstack/echo/v4"
 )
 
-func InitializeServer(cfg *config.Config) *echo.Echo {
+func InitializeServer(cfg *config.Config, db *gorm.DB) *echo.Echo {
 	server := echo.New()
 	logging.SetupLogger(server)
 	RegisterEndpoints(server)
--- services/Hangout/main.go
+++ services/Hangout/main.go
@@ -1,19 +1,65 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Ernestgio/Hangout-Planner/services/Hangout/internal/config"
+	"github.com/Ernestgio/Hangout-Planner/services/Hangout/internal/db"
 	"github.com/Ernestgio/Hangout-Planner/services/Hangout/internal/server"
 )
 
 func main() {
+	if err := Run(); err != nil {
+		log.Fatalf("Fatal error: %v", err)
+	}
+}
+
+func Run() error {
+	// Load configuration
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("Error loading config: %v", err)
 	}
 
-	e := server.InitializeServer(cfg)
+	// Connect to the database
+	ctxDB, cancelDB := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelDB()
+	dbConn, dbCloser, err := db.Connect(ctxDB, cfg)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := dbCloser(); err != nil {
+			log.Printf("Error closing database connection: %v", err)
+		}
+	}()
 
-	e.Logger.Fatal(e.Start(":" + cfg.AppPort))
+	// Initialize and start the server
+	e := server.InitializeServer(cfg, dbConn)
+	go func() {
+		if err := e.Start(":" + cfg.AppPort); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	//Perform a graceful server shutdown with a timeout
+	ctxServer, cancelServer := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelServer()
+
+	if err := e.Shutdown(ctxServer); err != nil {
+		return err
+	}
+
+	return nil
 }
