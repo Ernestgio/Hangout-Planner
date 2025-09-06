7cc96ed2356c1f951f69146abb7b786ee23844a4
Ernestgio <giovann.ernest@gmail.com>
2025-09-06 20:00:27 +0700

    added comments

--- services/Hangout/internal/constants/app.go
+++ services/Hangout/internal/constants/app.go
@@ -1,8 +1,10 @@
 package constants
 
 const (
+	// Environment constants
 	ProductionEnv = "PROD"
 
+	// Application constants
 	LoggerFormat = "[${time_rfc3339}] ${method} ${host}${uri} ${status} ${latency_human}\n"
 
 	//Status constants
