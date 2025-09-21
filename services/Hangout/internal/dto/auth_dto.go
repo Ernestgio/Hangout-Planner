0f7cdca7ee7aefd157798d489dba6859abfba89f
Ernestgio <giovann.ernest@gmail.com>
2025-09-21 22:54:43 +0700

    added sign in request dto

--- services/Hangout/internal/dto/auth_dto.go
+++ services/Hangout/internal/dto/auth_dto.go
@@ -5,3 +5,8 @@ type SignUpRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password"`
 }
+
+type SignInRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
