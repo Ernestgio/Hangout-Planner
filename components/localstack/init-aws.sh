181b36b534b2f13633f6fa85a435391cfea598e8
Ernestgio <giovann.ernest@gmail.com>
2026-02-13 10:01:36 +0700

    grafana tempo setup

--- components/localstack/init-aws.sh
+++ components/localstack/init-aws.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# LocalStack init hook to create S3 bucket on startup
+# LocalStack init hook to create S3 buckets on startup
 echo "Creating S3 bucket: hangout-files"
 if awslocal s3 mb s3://hangout-files --region ap-southeast-1 2>&1; then
-  echo "Bucket created successfully"
+  echo "Bucket hangout-files created successfully"
 else
-  echo "Bucket already exists or creation failed (this is OK if bucket exists)"
+  echo "Bucket hangout-files already exists or creation failed (this is OK if bucket exists)"
 fi
+
+echo "Creating S3 bucket: hangout-traces"
+if awslocal s3 mb s3://hangout-traces --region ap-southeast-1 2>&1; then
+  echo "Bucket hangout-traces created successfully"
+else
+  echo "Bucket hangout-traces already exists or creation failed (this is OK if bucket exists)"
+fi
+
 echo "S3 initialization complete"
